ac7e3c2f3c82438dfd3691787fa00c475a06ec53
ElDzaabo <75880532+ElDzaabo@users.noreply.github.com>
2023-03-22 01:49:30 +0300

    Лампа, Дебаг сцена, ProBuilder
    - Добавлена модель Lamp_1
    - Сцена SampleScene переименована в DebugScene
    - Сцена DebugScene реорганизована
    - Добавлена аудиозапись ButtonPress_1
    - Добавлен пэкэдж ProBuilder
    - Добавлен скрипт ButtonPresser [WIP]
    - Немного изменён скрипт HandPresence [WIP]

--- Assets/Scripts/HandPresence.cs
+++ Assets/Scripts/HandPresence.cs
@@ -8,64 +8,55 @@ public class HandPresence : MonoBehaviour
     [SerializeField] private List<GameObject> controllerPrefabs;
     private InputDevice targetDevice;
     private GameObject spawnedController;
+    public InputDeviceCharacteristics controllerCharacteristics;
 
     void Start()
     {
         {
-            //При запуске void Start() шлем не успевает очухаться и мы пропускаем всё что там написано, поэтому
-            //запускаем коорутину, чтобы подождать пока шлем придёт в себя
             StartCoroutine(GetDevices(1.0f));
         }
+    }
+    IEnumerator GetDevices(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
 
-        IEnumerator GetDevices(float delayTime)
+        //Создаём список всех VR-устройтв
+        List<InputDevice> devices = new List<InputDevice>();
+
+        //Получаем характеристики всех устройств и записываем в список
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        //Если в списке больше одного устройства
+        if (devices.Count > 0)
         {
-            yield return new WaitForSeconds(delayTime);
-
-            //Получаем список всех VR-устройтв (шлем + контроллеры)
-            List<InputDevice> devices = new List<InputDevice>();
-
-            //Получаем в devices характеристику лишь правого контроллера
-            InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
-            InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
-
-
-            Debug.Log("Давай посмотрим, что у тебя там за сетапчик");
+            Debug.Log("Найдены устройства. Давай посмотрим, что у тебя там");
             foreach (var item in devices)
             {
-                //Отображаем в консоли название предмета + его характеристики
                 Debug.Log(item.name + item.characteristics);
             }
-            Debug.Log("Garbage!");
+            Debug.Log("Н-да ну и хлам!");
 
-            if (devices.Count > 0)
+            targetDevice = devices[0];
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+            if (prefab)
             {
-                targetDevice = devices[0];
-                //Выбираем модель рук, в зависимости от устройства, которым мы пользуемся
-                //При добавлении новых моделей быть внимательным: Название префаба = название устроства в юнити, иначе работать не будет
-                GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
                 Debug.Log(targetDevice.name);
-                //Нашли совпадение в названии, значит создаем соответствующий префаб в сцене
-                if (prefab)
-                {
-                    spawnedController = Instantiate(prefab, transform);
-                }
-                else
-                {
-                    Debug.LogError("Что за мусор ты на себя нацепил?");
-                    //Создаём унифицированный префаб контроллера (для этого он должен быть первый в листе)
-                    spawnedController = Instantiate(controllerPrefabs[0], transform);
-                }
+                spawnedController = Instantiate(prefab, transform);
+            }
+            else
+            {
+                Debug.Log(controllerPrefabs.Count);
+                Debug.LogError("У тебя какой-то палёный шлемак. Ищем универсальные руки..");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
             }
         }
     }
 
-
-    // Update is called once per frame
     void Update()
     {
 
         //Слушаем нажатия кнопки
-        if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton , out bool primaryButtonValue) && primaryButtonValue)
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
         {
             //Вывод значения в консоль о нажатии
             Debug.Log("Йоу йоу йоу я нажимаю на кнопшку. Основную!");
@@ -77,13 +68,13 @@ public class HandPresence : MonoBehaviour
             //Вывод значения в консоль о нажатии на триггер
             Debug.Log("Кто стреляет?" + triggerValue);
         }
-            
+
         //Слушаем джойстик на контроллере
         if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue) && primary2DAxisValue != Vector2.zero)
         {
             //Вывод значения в консоль о передвижении джойстика
             Debug.Log("Кручу я гачу верчу!" + primary2DAxisValue);
         }
-            
+
     }
 }
